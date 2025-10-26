8564f535375c4ceec20d73884d31843d72a48d23
noodlechoi <choime321@gmail.com>
2025-10-26 13:52:49 +0900

    Player ProcessInput() 수정

--- NGP_project/Client/GameScene.cpp
+++ NGP_project/Client/GameScene.cpp
@@ -62,42 +62,15 @@ void GameScene::ProcessInput()
 	// 코드 길어져서 포인터로 받기
 	InputManager* input = GET_SINGLE(InputManager);
 	// 연속 이동을 원하면 GetButton 사용 (키를 누르고 있는 동안 true)
-	if (input->GetButtonDown(KeyType::Left)) 
-	{
-		for (GameObjectRef object : _objects)
-		{
-			if (object->GetObjectType() == ObjectType::Player)
-				object->Left();
-		}
-	} 
-	
-	if (input->GetButtonDown(KeyType::Right)) 
-	{
-		for (GameObjectRef object : _objects)
-		{
-			if (object->GetObjectType() == ObjectType::Player)
-				object->Right();
-		}
-	} 
-	
-	if (input->GetButtonDown(KeyType::Up)) 
-	{
-		for (GameObjectRef object : _objects)
-		{
-			if (object->GetObjectType() == ObjectType::Player)
-				object->Up();
-		}
-	} 
-	
-	if (input->GetButtonDown(KeyType::Down)) 
-	{
-		for (GameObjectRef object : _objects)
-		{
-			if (object->GetObjectType() == ObjectType::Player)
-				object->Down();
+	for (GameObjectRef object : _objects) {
+		if (object->GetObjectType() == ObjectType::Player) {
+			if (input->GetButtonDown(KeyType::Left)) object->Left();
+			if (input->GetButtonDown(KeyType::Right)) object->Right();
+			if (input->GetButtonDown(KeyType::Up)) object->Up();
+			if (input->GetButtonDown(KeyType::Down)) object->Down();
 		}
 	}
-
+	
 	// 이동 버튼 GetButton으로 변경 시 주석 풀기
 	/*if (input->GetButtonUp(KeyType::Left) || input->GetButtonUp(KeyType::Right) || input->GetButtonUp(KeyType::Up) || input->GetButtonUp(KeyType::Down)) {
 		for (PlayerRef player : _players) {

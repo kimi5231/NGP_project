1e69dc69fd260bc8d1843860721ea920f6b017a1
Lim Sooyoung <djwjod@naver.com>
2025-11-09 21:53:57 +0900

    [Server] Recv에 필요한 Pakcet 정보 추가

--- NGP_project/Server/Packet.h
+++ NGP_project/Server/Packet.h
@@ -1,1 +1,17 @@
 #pragma once
+struct Header
+{
+    PacketID id;
+    int dataSize;
+};
+
+enum PacketID
+{
+    C_UpdateObjectState,
+    C_UpdateDir,
+    C_Move,
+    C_Collision,
+    C_UseItem,
+    C_StayGame,
+    C_EndGame,
+};

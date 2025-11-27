08f89ccd45ecde2d976b24ec871e50e67f98768a
Lim Sooyoung <djwjod@naver.com>
2025-11-28 00:34:37 +0900

    [Server] 필요없는 코드 삭제

--- NGP_project/Server/ServerFramework.cpp
+++ NGP_project/Server/ServerFramework.cpp
@@ -225,9 +225,6 @@ void ServerFramework::ProcessAccept(SOCKET clientSocket)
 
 	std::cout << "Client" << newClient->id << " 접속" << std::endl;
 
-	// 새로 접속한 Client에게 자기 자신을 나타내는 Player 정보 송신
-	SendAddObjectPacket(player);
-
 	// 새로 접속한 Client에게 Room에 있는 모든 Object 정보 송신
 	std::unordered_map<int, GameObjectRef> objects = _room->GetObjects();
 	for (const auto& item : objects)

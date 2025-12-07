cf9fdf05345fb212d754de0675825c25c5906286
Im caheeun <dev.chaeng@gmail.com>
2025-12-07 20:49:30 +0900

    [Client] 플레이어 죽으면 안 그리기, 로컬 플레이어 입력 막기 추가
    지금은 죽은 플레이어를 몬스터가 따라옴.. 서버에서 죽은 플레이어는 안 따라오도록 해야됨

--- NGP_project/Client/GameScene.cpp
+++ NGP_project/Client/GameScene.cpp
@@ -47,16 +47,15 @@ GameScene::~GameScene()
 void GameScene::Update()
 {
 	EnterCriticalSection(&g_cs);
-
 	if (!_localPlayer.get())
 	{
 		LeaveCriticalSection(&g_cs);
 		return;
 	}
-	
+
+	if (_localPlayer->GetState() != ObjectState::Dead)
+		ProcessInput();
 	LeaveCriticalSection(&g_cs);
-	
-	ProcessInput();
 
 	EnterCriticalSection(&g_cs);
 	for (const auto& object : _objects) {
@@ -121,14 +120,20 @@ void GameScene::Render(HDC hdc)
 
 	// Local Player
 	if (_localPlayer) {
-		_localPlayer->Render(memDC, memDCImage);
-		_localPlayer->GetBoundingBox().Render(memDC, memDCImage, RGB(255, 0, 0));
+		if (_localPlayer->GetState() != ObjectState::Dead)
+		{
+			_localPlayer->Render(memDC, memDCImage);
+			_localPlayer->GetBoundingBox().Render(memDC, memDCImage, RGB(255, 0, 0));
+		}
 	}
 
 	// Other Player
 	for (const auto& [id, player] : _players) {
-		player->Render(memDC, memDCImage);
-		player->GetBoundingBox().Render(memDC, memDCImage, RGB(0, 255, 0));	// 디버깅용
+		if (player->GetState() != ObjectState::Dead)
+		{
+			player->Render(memDC, memDCImage);
+			player->GetBoundingBox().Render(memDC, memDCImage, RGB(0, 255, 0));	// 디버깅용
+		}
 	}
 
 	// Monster
